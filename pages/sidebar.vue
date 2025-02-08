189e3197ec3b7bbbab2b768440a67cd36891cae5
Ibrahim Konsowa <ibrahim.konsowa@saab.ae>
2025-02-08 06:44:47 +0400

    sync

---
+++ pages/sidebar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import SidebarProvider from "~/components/ui/sidebar/SidebarProvider.vue";
+    import Sidebar from "~/components/ui/sidebar/Sidebar.vue";
+
+</script>
+
+<template>
+    <SidebarProvider>
+        <div class="sidebar">
+            <Sidebar>
+                <SidebarHeader>
+                    Some content in the header
+                </SidebarHeader>
+
+                <SidebarContent>
+                    Some content in the content
+                </SidebarContent>
+
+                <SidebarFooter>
+                    Some content in the footer
+                </SidebarFooter>
+            </Sidebar>
+        </div>
+    </SidebarProvider>
+</template>
+
+<style scoped>
+
+</style>
